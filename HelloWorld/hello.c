1bd1fd1c97512cae24dc46aad2842f0ec84e12e2
tiagokapp <33268117+tiagokapp@users.noreply.github.com>
2017-10-31 21:51:52 -0200

    Update hello.c

--- HelloWorld/hello.c
+++ HelloWorld/hello.c
@@ -3,5 +3,20 @@
 int main()
 {
   puts("Hello, World!");
+  
+  	int idade,cpf;
+	printf("\n\nHELLO WORLD");
+	printf("qual a sua idade");
+	scanf("%d",&idade);
+	printf("qual o sei cpf");
+	scanf("%d",&cpf);
+	
+	
+	
+	printf(" \n\nsua idade e cpf sao :%d %d",idade,cpf);
+	
+  
+  
+  
   return 0;
 }

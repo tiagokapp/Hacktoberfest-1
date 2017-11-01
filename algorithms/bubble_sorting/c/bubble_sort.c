eb97c6a6247ee9bcfe7b1da01608b76b3d6b41f3
tiagokapp <33268117+tiagokapp@users.noreply.github.com>
2017-10-31 23:13:43 -0200

    Update bubble_sort.c

--- algorithms/bubble_sorting/c/bubble_sort.c
+++ algorithms/bubble_sorting/c/bubble_sort.c
@@ -8,26 +8,69 @@
 
 
 
-void bubbleSort(int arr[], int n)
-{
-   int i, j;
-   for (i = 0; i < n-1; i++)      
- 
-       // Last i elements are already in place   
-       for (j = 0; j < n-i-1; j++) 
-           if (arr[j] > arr[j+1])
-              swap(&arr[j], &arr[j+1]);
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define n 10
+
+int list[n] = {1,8,4,6,0,3,5,2,7,9};
+
+int i = 0;
+bool switched = true;
+
+void display() {
+   int i;
+   printf("[");
+	
+
+   for(i = 0; i < n; i++) {
+      printf("%d ",list[i]);
+   }
+	
+   printf("]\n");
 }
 
-int main()
-{
-int i,j,k;
-int a[5];
-for(i=0;i<5;i++)
-scanf("%d",&a[i]);
-bubblesort(a,5);
-for(i=0;i<5;i++)
-printf("%d\n",a[i]);
+void bubbleSort() {
+   int temp;
+   int j;
+   //bool switched;
+   //i = 0;
+   //for(i = 0; i < n-1; i++) { 
+   if(i == n - 1 && switched == false) return;//solucao trivial
+    //if(i == n - 1) return;
 
-return 0;
+    switched = false;		
+   for(j = 0; j < n-i-1; j++) {
+        printf("     Items compared: [ %d, %d ] ", list[j],list[j+1]);
+			
+        if(list[j] > list[j+1]) {
+           switched = true;
+           temp = list[j];
+           list[j] = list[j+1];
+           list[j+1] = temp;
+
+           printf(" => swapped [%d, %d]\n",list[j],list[j+1]);
+        }else {
+           printf(" => not swapped\n");
+        }
+		
+     }//end for
+      
+      //printf("Iteration %d#: ",(i+1)); 
+      //display();
+        i = i + 1;
+     
+    bubbleSort();
+	
+}
+
+void main() {
+   printf("Input Array: ");
+   display();
+   printf("\n");
+	
+   bubbleSort();
+   printf("\nOutput Array: ");
+   display();
 }

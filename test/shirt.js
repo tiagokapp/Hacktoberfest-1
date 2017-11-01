eb0be534395dae72aca45a199b2c102a52ffcb3b
tiagokapp <33268117+tiagokapp@users.noreply.github.com>
2017-10-31 22:39:54 -0200

    Update shirt.js

--- test/shirt.js
+++ test/shirt.js
@@ -1,4 +1,35 @@
-{
-   "pull_request": "yes",
-   "pull_request_count": 0
+#include<stdio.h>
+
+
+int fatorial( int num);
+
+
+main(){
+	
+	int resp, val;
+	printf("digite o valor do fatorial \n\n");
+	scanf("%d", &val);
+	
+	resp=fatorial(val);
+	printf("\n\no fatorial eh %d", resp);
+	
+	
+	return 0;
 }
+
+
+int fatorial (int num){
+	
+	if (num==0){
+		return 1;
+		
+		
+	}
+	
+	else {
+	
+	return num*(fatorial(num-1));
+	
+	
+	
+	}
